7bbbe90d8102b2d1a976ec5d286e3cda2b53956d
shreeniwaspoudel <90775736+shreeniwaspoudel@users.noreply.github.com>
2022-10-08 14:00:23 -0500

    first commit

---
+++ openFile.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  8 13:38:23 2022
+
+@author: ShreeniwasSharma-Pou
+"""
+
+f = open("demofile.txt", "r+")
+
+number = f.readlines()[-1]
+
+number = int(number)
+
+number += 1
+
+f.writelines(str(number) + "\n")
+
+f.close()
+
