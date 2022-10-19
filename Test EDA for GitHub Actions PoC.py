1e766a4aa317014870e3d9e398b01991989f2010
shreeniwaspoudel <90775736+shreeniwaspoudel@users.noreply.github.com>
2022-10-19 16:48:31 -0500

    Update Test EDA for GitHub Actions PoC.py

--- Test EDA for GitHub Actions PoC.py
+++ Test EDA for GitHub Actions PoC.py
@@ -9,12 +9,12 @@ from sentence_transformers import SentenceTransformer, util
 
 
 # Reading 3rd Sept 2022 week's data
-week_0903 = pd.read_excel('C:/Doyle_3rd Sept 2022/tweet_text_input.tcc_ill_one_week_20220903.xlsx') 
+week_0903 = pd.read_excel('tweet_text_input.tcc_ill_one_week_20220903.xlsx') 
 week_0903.drop(['Unnamed: 6', 'Unnamed: 9'], axis=1, inplace=True)
 week903 = week_0903.copy(deep=True)
 
 # Reading 10th Sept 2022 week's data
-week_0910 = pd.read_excel('C:/tweet_text_composite.tcc_ill_one_week_20220910.xlsx')
+week_0910 = pd.read_excel('tweet_text_composite.tcc_ill_one_week_20220910.xlsx')
 week_0910.drop(['Unnamed: 6', 'Unnamed: 9'], axis=1, inplace=True)
 week910 = week_0910.copy(deep=True)
 
